2a7141b9e9959fa6e8e6ea06c609de7a05b2a373
JennyLottner <jenny.lottner@gmail.com>
2025-04-18 14:51:06 +0300

    Add app general layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Properties</h1>
+  <div class="grid grid-rows-[3rem_1fr_2rem] min-h-screen">
+    <header class="bg-gray-800 text-white text-center p-2.5 sticky top-0 z-1">
+      <h1>Properties</h1>
+    </header>
+
+    <main class="bg-gray-100 p-4 overflow-auto">
+    </main>
+
+    <footer class="bg-gray-800 text-white text-center sticky bottom-0">
+      <p>© Jenny Tover</p>
+    </footer>
+  </div>
 </template>
 
 <script>
