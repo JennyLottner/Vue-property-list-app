b0123fc8f829cb6a618ce18c0ea2c5051f4fef53
JennyLottner <jenny.lottner@gmail.com>
2025-04-18 15:17:47 +0300

    Create PropertyPreview component

--- src/components/PropertyList.vue
+++ src/components/PropertyList.vue
@@ -1,18 +1,24 @@
 <template>
-  <ul class="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] auto-rows-auto gap-4 mt-2" >
-    <li v-for="property in properties" :key="property.id" class="bg-blue-200 rounded flex relative">
-      {{ property.name }}
-    </li>
-  </ul>
-</template>
-
-<script>
-export default {
+    <ul class="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] auto-rows-auto gap-4 mt-2">
+      <li v-for="property in properties" :key="property.id" class="bg-blue-200 rounded flex relative">
+          <PropertyPreview :property="property"/>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  import PropertyPreview from './PropertyPreview.vue';
+  
+  export default {
   props: {
-    properties: { type: Array, required: true },
+      properties: { type: Array, required: true},
   },
-}
-</script>
-
-<style>
-</style>
+  components: {
+      PropertyPreview,
+  },
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ src/components/PropertyPreview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col p-2">
+      <img
+        :src="property.imgUrl"
+        :alt="property.id"
+        class="aspect-5/3 object-cover rounded"
+      />
+      <strong class="text-lm w-3/4">{{ property.name }}</strong>
+      <p class="text-sm"><strong>Price:</strong> {{ property.price }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      property: { type: Object, required: true },
+    },
+    inheritAttrs: false,
+  }
+  </script>
+  
+  <style>
+  </style>
